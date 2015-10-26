12e54659b05efcf7a2af89c4b7a8193fa5815e4c
Robert Sell <sellcorp.llc@gmail.com>
2015-10-25 20:52:15 -0400

    Added map transform stream and registered it in index.js. Currently passes all tests.

---
+++ index.js
@@ -0,0 +1,6 @@
+/*jslint vars: true, devel:true, nomen: true, node: true, indent: 4, maxerr: 50*/
+
+module.exports = {
+    ObjectRStream: require("./lib/objectrstream/object.read.stream.js"),
+    MapTStream: require("./lib/maptsream/map.transform.stream.js")
+};
