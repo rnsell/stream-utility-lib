9d0915d97e68f2a1f58e704e220b3c949da8b9c5
unknown <rsell@WA088L.PATRIOTFCU.ORG>
2015-11-17 16:05:47 -0500

    Fixed tests for counter and filter.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /*jslint vars: true, devel:true, nomen: true, node: true, indent: 4, maxerr: 50*/
 'use strict';
 
-module.exports.ArraySplit = require("./lib/arraysplit/arraysplit.transform.stream.js");
-module.exports.Counter = require("./lib/counter/counter.transform.stream.js");
-module.exports.Filter = require("./lib/filter/filter.transform.stream.js");
-module.exports.FilterCollector = require("./lib/filtercollector/filtercollector.transform.stream.js");
-module.exports.Map = require("./lib/map/map.transform.stream.js");
+module.exports.ArraySplitStream = require("./lib/arraysplit/arraysplit.transform.stream.js");
+module.exports.CounterStream = require("./lib/counter/counter.transform.stream.js");
+module.exports.FilterStream = require("./lib/filter/filter.transform.stream.js");
+module.exports.FilterCollectorStream = require("./lib/filtercollector/filtercollector.transform.stream.js");
+module.exports.MapStream = require("./lib/map/map.transform.stream.js");
 module.exports.ObjectReadStream = require("./lib/object/object.read.stream.js");
-module.exports.Reduce = require("./lib/reduce/reduce.transform.stream.js");
+module.exports.ReduceStream = require("./lib/reduce/reduce.transform.stream.js");
