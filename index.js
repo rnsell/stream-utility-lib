508c50003bbc760da773dc4d8074c61e59fe16bd
unknown <rsell@WA088L.PATRIOTFCU.ORG>
2015-11-17 15:18:19 -0500

    Changes folder names.

---
+++ index.js
@@ -0,0 +1,10 @@
+/*jslint vars: true, devel:true, nomen: true, node: true, indent: 4, maxerr: 50*/
+'use strict';
+
+module.exports.ArraySplit = require("./arraysplit/arraysplit.transform.stream.js");
+module.exports.Counter = require("./counter/counter.transform.stream.js");
+module.exports.Filter = require("./filter/filter.transform.stream.js");
+module.exports.FilterCollector = require("./filtercollector/filtercollector.transform.stream.js");
+module.exports.Map = require("./map/map.transform.stream.js");
+module.exports.ObjectReadStream = require("./object/object.read.stream.js");
+module.exports.Reduce = require("./reduce/reduce.transform.stream.js");
